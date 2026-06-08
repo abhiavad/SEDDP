8e39573e3c8da950792969b2a00dd37d40f70378
TjallingHuson <tdhuson@gmail.com>
2026-06-08 11:08:22 +0200

    changed 4 int32 to int16 to save space

--- Horizon_detection_C++/HorizonTypes.hpp
+++ Horizon_detection_C++/HorizonTypes.hpp
@@ -2,15 +2,16 @@
 #pragma once
 #include <stdint.h>
 
-// Matches the 6-column Python .bin file structure
-struct LookupEntry {
-    int32_t table_roll;  // Col 0: True Roll (Scaled by 4096)
-    int32_t table_pitch; // Col 1: True Pitch (Scaled by 4096)
-    int32_t v_x;         // Col 2: (Scaled by 4096)
-    int32_t v_y;         // Col 3: (Scaled by 4096)
-    int32_t area;        // Col 4: (Scaled by 4096)
-    int32_t atan2_val;   // Col 5: Roll estimation for search (Scaled by 4096)
+// Adding __attribute__((packed)) ensures the compiler doesn't add padding bytes
+struct __attribute__((packed)) LookupEntry {
+    int32_t table_roll;   // 4 bytes
+    int32_t table_pitch;  // 4 bytes
+    int16_t v_x;          // 2 bytes
+    int16_t v_y;          // 2 bytes
+    int16_t area;         // 2 bytes
+    int16_t atan2_val;    // 2 bytes
 };
+
 
 // The final output of the manager
 struct HorizonOutput {
